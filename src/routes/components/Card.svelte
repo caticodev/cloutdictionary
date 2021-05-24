d910f913fd585c8e8e362437344bea51d2ae4191
catico <kathy0kate@gmail.com>
2021-05-24 14:27:01 +0200

    🚀

--- src/routes/components/Card.svelte
+++ src/routes/components/Card.svelte
@@ -9,10 +9,12 @@
 			<h2 class="font-black font-mono my-8 text-primary tracking-wider text-3xl">
 				{post.phrase}
 			</h2>
-			<p class="font-light text-lg">
+			<p class="font-light text-lg tracking-wide">
 				{post.definition}
 			</p>
-			<p class="font-light mt-8 text-lg inline-flex items-center italic">{post.usage}</p>
+			<p class="font-light mt-8 text-lg tracking-wide inline-flex items-center italic">
+				{post.usage}
+			</p>
 		</div>
 	</div>
 {/if}
