5f6c9352bf038ed52cbd8f745220ffc1c6bb9799
catico <kathy0kate@gmail.com>
2021-06-23 18:47:53 +0200

    🔧

--- windi.config.ts
+++ windi.config.ts
@@ -9,7 +9,6 @@ export default {
       fontFamily: {
         display: 'PT Mono,ui-monospace,SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
         body: 'PT Sans,ui-sans-serif,system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
-
       }
     },
   },
